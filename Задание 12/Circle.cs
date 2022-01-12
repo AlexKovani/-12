687df0721c68fbfcb8c992d8572fe878fc3d60ce
canya <canya@DESKTOP-0RBMO97>
2022-01-12 22:47:13 +0300

    Добавьте файлы проекта.

---
+++ Задание 12/Circle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_12
+{
+    // Разработать статический класс для работы с окружностью.Класс должен содержать 3 метода:
+    // метод, определяющий длину окружности по заданному радиусу;
+    // метод, определяющий площадь круга по заданному радиусу;
+    // метод, проверяющий принадлежность точки с координатами(x, y) кругу с радиусом r и координатами центра x0, y0.
+    static class Circle
+    {
+      
+        public static void Length(int r)  //длинa C = 2 * PI * r
+        {
+            if (r > 0)
+            {
+                double C = 2 * Math.PI * r;
+                Console.WriteLine("Длина окружности = {0}", C);
+            }
+            else
+            {
+                if (r < 0)
+                {
+                    Console.WriteLine("Значение радиуса должно быть больше 0!");
+                }
+                if (r == 0)
+                {
+                    Console.WriteLine("Радиус = 0. Это точка. Попробуйте увеличить значение радиуса!");
+                }
+            }
+        }
+
+        public static void Square(int r) //площадь S = PI*r^2
+        {
+            if (r > 0)
+            {
+                double S = Math.PI * Math.Pow(r, 2);
+                Console.WriteLine("Длина круга = {0}", S);
+            }
+            else
+            {
+                if (r < 0)
+                {
+                    Console.WriteLine("Значение радиуса должно быть больше 0!");
+                }
+                if (r == 0)
+                {
+                    Console.WriteLine("Радиус = 0. Это точка. Попробуйте увеличить значение радиуса!");
+                }
+            }
+           
+        }
+
+        public static void Points(int x1, int x2, int y1, int y2, int r)  //точкa с координатами x1, y1 
+        {
+            double a = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + r);
+            if (a < r)
+            {
+                Console.WriteLine("Точка принадлежит данному кругу!");
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит данному кругу!");
+            }
+        }
+    }
+}
---
+++ Задание 12/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_12
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Я калькулятор параметров окружности!");
+            Console.WriteLine("Что хотите вычислить?");
+            Console.WriteLine("1 - Длину\r\n2 - Площадь\r\n3- Принадлежность точки с координатома х и у");
+            Console.Write("Введите значение: ");
+            int vvod;
+            try
+            {
+                vvod = Convert.ToInt32(Console.ReadLine());
+                if (vvod <= 0 || vvod >3)
+                {
+                    Console.WriteLine("Вы ввели неверное число!");
+                }
+                switch (vvod)
+                {
+                    case 1:
+                        {
+                            Console.Write("Введите радиус: ");
+                            int r = Convert.ToInt32(Console.ReadLine());
+                            Circle.Length(r);
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("Введите радиус: ");
+                            int r = Convert.ToInt32(Console.ReadLine());
+                            Circle.Square(r);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Введите координаты точки через Enter: ");
+                            int x1 = Convert.ToInt32(Console.ReadLine());
+                            int y1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Введите координаты центра окружности через Enter: ");
+                            int x2 = Convert.ToInt32(Console.ReadLine());
+                            int y2 = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Введите радиус: ");
+                            int r = Convert.ToInt32(Console.ReadLine());
+                            Circle.Points(x1, x2, y1, y2, r);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
